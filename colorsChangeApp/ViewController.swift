966b47062ce3189119e04003287cfed3c4456f01
Kos <mahovk1@gmail.com>
2020-08-23 13:26:57 +0300

    Initial Commit

---
+++ colorsChangeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  colorsChangeApp
+//
+//  Created by Konstantin on 23.08.2020.
+//  Copyright © 2020 Konstantin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
